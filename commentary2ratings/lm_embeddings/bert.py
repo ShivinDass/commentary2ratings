42f1668cd9421dee10b3de40227a9f524eef19fd
Shivin Dass <shivindass@gmail.com>
2022-10-09 14:15:19 -0700

    files

---
+++ commentary2ratings/lm_embeddings/bert.py
@@ -0,0 +1,22 @@
+''' 
+Load BERT from hugging face and generate embeddings for commentaries.
+Visualize commentaries using t-SNE plot.
+'''
+
+def load_model():
+	pass
+
+def load_data():
+	pass
+
+def embed_commentaries():
+	pass
+
+def visualize_commenataries():
+	pass
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
