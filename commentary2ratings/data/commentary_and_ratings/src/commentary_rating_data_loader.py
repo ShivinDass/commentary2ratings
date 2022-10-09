12a323f6901b0226e2a2fe37b5852d2e288ea31a
Shivin Dass <shivindass@gmail.com>
2022-10-09 14:17:59 -0700

    missing added

---
+++ commentary2ratings/data/commentary_and_ratings/src/commentary_rating_data_loader.py
@@ -0,0 +1,8 @@
+# A dataloader matching player ratings to the commentaries from the corresponding matches
+
+from torch.utils.data import DataLoader
+
+class CommentaryAndRatings(Dataset):
+
+	def __init__(self):
+		pass
