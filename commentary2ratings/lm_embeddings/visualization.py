1673e49823ab68885086f694e814bdfd5c20273e
sanromra <sanromra@gmail.com>
2022-10-15 23:13:53 -0700

    Fixed visualization problems

--- commentary2ratings/lm_embeddings/visualization.py
+++ commentary2ratings/lm_embeddings/visualization.py
@@ -4,10 +4,11 @@ import seaborn as sns
 import pandas as pd
 
 def obtain_visualization(embeddings, ground_truth):
-	tsne_result_df = pd.DataFrame({'tsne_1': embeddings[:,0], 'tsne_2': embeddings[:,1], 'label': ground_truth})
+	X_embedded = TSNE(n_components=2, learning_rate='auto', init='random', perplexity=5).fit_transform(embeddings)
+	tsne_result_df = pd.DataFrame({'tsne_1': X_embedded[:,0], 'tsne_2': X_embedded[:,1], 'label': ground_truth})
 	fig, ax = plt.subplots(1)
 	sns.scatterplot(x='tsne_1', y='tsne_2', hue='label', data=tsne_result_df, ax=ax,s=120)
-	lim = (embeddings.min()-5, embeddings.max()+5)
+	lim = (X_embedded.min()-5, X_embedded.max()+5)
 	ax.set_xlim(lim)
 	ax.set_ylim(lim)
 	ax.set_aspect('equal')
