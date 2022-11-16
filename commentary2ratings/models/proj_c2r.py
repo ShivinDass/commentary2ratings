2a4fb134f0c23d2f146efc295fc8396180e45b20
Indra Kumar <36892073+Indra24710@users.noreply.github.com>
2022-11-16 15:10:00 -0800

    Update proj_c2r.py

--- commentary2ratings/models/proj_c2r.py
+++ commentary2ratings/models/proj_c2r.py
@@ -5,9 +5,30 @@ class ProjC2R(BaseModel):
 
     def __init__(self):
         super().__init__()
+        self.encoder_layer=nn.Sequential(nn.Linear(768, 512),
+                        nn.BatchNorm1d(512),
+                        nn.LeakyReLU(0.2),
+                        nn.Linear(512, 256),
+                        nn.BatchNorm1d(256),
+                        nn.LeakyReLU(0.2),
+                        nn.Linear(256, 128),
+                        nn.BatchNorm1d(128),
+                        nn.LeakyReLU(0.2),
+                        nn.Linear(128,64),
+                        nn.LeakyReLU(0.2))
+        self.regression_layer=nn.Linear(64+46,2)
     
     def forward(self, inputs):
-        pass
+        batch, seq = inputs['padded_commentary_embedding'].shape[:2]
+        inp_sizes = inputs['commentary_len'].type(torch.long)
+        encoded = self.encoder_layer(inputs['padded_commentary_embedding'].view(batch*seq, -1)).reshape(batch, seq, -1)
+        embeddings = torch.sum(encoded, dim=1)/inp_sizes[:,None]
+        embedding_and_player = torch.cat((embeddings, inputs['player_stats']), dim=-1)
+        return self.regression_layer(embedding_and_player)
+        
     
     def loss(self, outputs, inputs):
-        pass
+        mu, log_sigma = outputs[:, 0], outputs[:, 1]
+        sigma = log_sigma.exp()
+        loss = -1*(-1*((inputs['rating'] - mu) ** 2) / (2 * sigma**2) - log_sigma - math.log(math.sqrt(2*math.pi)))
+        return torch.mean(loss)
