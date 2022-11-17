951ab62446d1093fc87f2733426b93abf291190f
Indra Kumar <36892073+Indra24710@users.noreply.github.com>
2022-11-17 15:10:29 -0800

    Update proj2r with dropout and batchnorm layers

--- commentary2ratings/models/proj_c2r.py
+++ commentary2ratings/models/proj_c2r.py
@@ -5,26 +5,34 @@ class ProjC2R(BaseModel):
 
     def __init__(self):
         super().__init__()
-        self.encoder_layer=nn.Sequential(nn.Linear(768, 512),
+        self.encoder_layer=nn.Sequential(nn.Linear(768+452, 512),
+                        nn.ReLU(0.2),
+                        nn.Dropout1d(0.2),
                         nn.BatchNorm1d(512),
-                        nn.LeakyReLU(0.2),
                         nn.Linear(512, 256),
+                        nn.ReLU(0.2),
+                        nn.Dropout1d(0.2),
                         nn.BatchNorm1d(256),
-                        nn.LeakyReLU(0.2),
                         nn.Linear(256, 128),
-                        nn.BatchNorm1d(128),
-                        nn.LeakyReLU(0.2),
-                        nn.Linear(128,64),
-                        nn.LeakyReLU(0.2))
-        self.regression_layer=nn.Linear(64+46,2)
+                        nn.ReLU(0.2))
+        self.regression_layer=nn.Sequential(nn.Linear(128, 64),
+                        nn.ReLU(0.2),
+                        nn.Dropout1d(0.2),
+                        nn.BatchNorm1d(64),
+
+                        nn.Linear(64,32),
+                        nn.ReLU(0.2),
+                        
+                        nn.Linear(32,2))
     
     def forward(self, inputs):
         batch, seq = inputs['padded_commentary_embedding'].shape[:2]
-        inp_sizes = inputs['commentary_len'].type(torch.long)
-        encoded = self.encoder_layer(inputs['padded_commentary_embedding'].view(batch*seq, -1)).reshape(batch, seq, -1)
-        embeddings = torch.sum(encoded, dim=1)/inp_sizes[:,None]
-        embedding_and_player = torch.cat((embeddings, inputs['player_stats']), dim=-1)
-        return self.regression_layer(embedding_and_player)
+        conc_inp = torch.cat((inputs['padded_commentary_embedding'].view(batch*seq, -1), torch.repeat_interleave(inputs['player'], seq, dim=0)), dim=-1)
+        projections = self.encoder_layer(conc_inp).reshape(batch, seq, -1)
+        mask = torch.norm(inputs['padded_commentary_embedding'], dim=-1) > 0
+        projections = torch.sum(projections*(mask[..., None]), dim=1)/(inputs['commentary_len'][:, None])
+        return self.regression_layer(projections)
+        
         
     
     def loss(self, outputs, inputs):
